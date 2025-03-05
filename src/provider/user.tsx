f2407cd44caeb8dae9727cc5a383bee653fa79cd
buzooka <bio.local.tech@gmail.com>
2025-03-05 20:27:41 +0530

    Re-added files

---
+++ src/provider/user.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { UserContext } from "@/context/user";
+import type { User } from "@/types";
+
+export function UserProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = React.useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      {children}
+    </UserContext.Provider>
+  );
+}
